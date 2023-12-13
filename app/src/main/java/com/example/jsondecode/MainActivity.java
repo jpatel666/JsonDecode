cd62d0b5557456745618d0b834b10ac4fad67573
Jimmy <jhpatel392@gmail.com>
2023-12-13 07:37:33 -0800

    JsonDecode

--- app/src/main/java/com/example/jsondecode/MainActivity.java
+++ app/src/main/java/com/example/jsondecode/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.jsondecode;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,11 +14,31 @@ public class MainActivity extends AppCompatActivity {
 
     //JSON Check:-JsonViewer And JsonValidator
 
+    String str = "{  \n" +
+            "    \"employee\": {  \n" +
+            "        \"name\":       \"sonoo\",   \n" +
+            "        \"salary\":      56000,   \n" +
+            "        \"married\":    true  \n" +
+            "    }  \n" +
+            "}  ";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        try {
+            JSONObject jsonObject = new JSONObject(str);  //Alt+Enter
 
+            JSONObject employee = jsonObject.getJSONObject("employee");
+            String name = employee.getString("name");
+            int salary = employee.getInt("salary");
+            boolean married = employee.getBoolean("married");
+
+            Log.e("Demo",""+employee);
+
+        } catch (JSONException e) {
+
+        }
     }
 }
